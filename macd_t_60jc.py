a8c6c54d62b96ad53d900fc92c91028330d39e69
ocean998 <40862700+ocean998@users.noreply.github.com>
2019-03-12 16:07:07 +0800

    计算60分钟金叉，模块macd_t_60jc.py

--- macd_t_60jc.py
+++ macd_t_60jc.py
@@ -3,5 +3,6 @@ import macd_base as mb
 
 
 if __name__ == "__main__":
+    # 60 即将金叉
     macd_60 = mb.MACD_INDEX('60')
     macd_60.save_bing_golden('all', False)
--- macd_test_dbl.py
+++ macd_test_dbl.py
@@ -5,7 +5,7 @@ import macd_base as mb
 
 
 if __name__ == '__main__':
-    macd_60 = MACD_INDEX('60')
+    macd_60 = mb.MACD_INDEX('60')
     # macd_60.set_time('2018-06-01','2018-12-11')
 
     macd_60.save_bottom('all', False)
